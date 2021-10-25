7ad4f6391f65626fc893e665d463bbf8d155977a
Faitly <fty.57@alu.ufc.br>
2021-10-25 10:17:32 -0300

    70. Componente Bandeira (Flag) #01

--- minefieldApp/src/components/Field.js
+++ minefieldApp/src/components/Field.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { View, StyleSheet, Text} from 'react-native'
 import params from '../params'
 import Mine from "./Mine"
+import Flag from "./Flag"
 
 export default props => {
      // Utilização do destructuring
-     const { mined, opened, nearMines, exploded } = props 
+     const { mined, opened, nearMines, exploded, flagged } = props 
 
      // Adição de estilos
      const styleField = [styles.field]
      if(opened) styleField.push(styles.opened)
      if(exploded) styleField.push(styles.exploded)
-     if(styleField.length === 1) styleField.push(styles.regular)
+     if(flagged) styleField.push(styles.flagged) 
+     if(!opened && !exploded) styleField.push(styles.regular)
 
      // A cor será sensivel a quantidade de minas que estão ao redor
      let color = null
@@ -26,8 +28,9 @@ export default props => {
           <View style={styleField}>
                { !mined  && opened && nearMines > 0 ? 
                     <Text style={[styles.label, {color: color}]}>
-                         {nearMines}</Text>: false }
+                         {nearMines}</Text>: false}
                {mined && opened? <Mine/> : false}
+               {flagged && !opened? <Flag/>: false}
           </View>
      )
 }
