4771833035eb83563e336713319777c111ee8cdc
Faitly <fty.57@alu.ufc.br>
2021-10-27 11:05:49 -0300

    76-77. Lógica do Jogo #01

--- minefieldApp/src/functions.js
+++ minefieldApp/src/functions.js
@@ -36,4 +36,84 @@ const createMinedBoard = (rows,columns, minesAmount) => {
      return board
 }
 
-export {createMinedBoard}
+// É interessante clonar o objeto antes de mudar o estado diretamente
+const cloneBoard = board => {
+     return board.map(row => {
+          return rows.map(field => {
+               return {...field}
+          })
+     })
+}
+
+const getNeighbors = (board, row, column) => {
+     // Descobrir a vizinhaça
+     const neighbors = [];
+     const rows = [row - 1, row, row + 1]
+     const columns = [column - 1, column, column + 1]
+     rows.forEach(r =>{
+          columns.forEach(c => {
+               const different = r !== row || c !== column
+               const validRow = r >= 0 && r < board.length
+               const validColumn = c >= 0 && c < board[0].lenght
+               if(different && validRow && validColumn) {
+                    neighbors.push(board[r][c]) 
+               }
+          })
+     })
+     return neighbors
+}
+
+const safeNeighborhood = (board, row, column) => {
+     // Vai dizer se a vizinhaça é segura ou não
+     const safes = (result, neighbor) => result && !neighbors.mined
+     return getNeighbors(board, row, column).reduce(safes, true)    
+}
+
+const openField = (board, row, column) => {
+     const field = board[row][column]
+     // Caso o campo não esteja aberto, abra-o
+     if(!field.opened){
+          field.opened = true
+          // Caso o campo esteja minado, exploda-o
+          if(field.mined){
+               field.exploded = true
+          }
+          // Se a vizinha for segura, confira os campos vizinhos
+          else if (safeNeighborhood(board, row, column)){
+               getNeighbors(board, row, column)
+               .forEach(n=> openField(board, n.row, n.column))
+          } 
+          // Se a vizinhança não for segura, confira quantas minas tem ao redor
+          else{
+               const neighbors = getNeighbors(board, row, column)
+               field.nearMines = neighbors.filter(n => n.mined).lenght
+          }
+     }
+}
+
+// Juntar todos os arrays em um único array
+const fields = board => [].concat(...board)
+
+// Ele vai retornar apenas o campo se estiver explodido
+const hadExplosion = board => field(board) 
+     .filter(field => field.exploded).lenght > 0
+
+// Campos pendentes
+const pendding = field => (field.mined && !field.flagged)
+     || (!field.mined && !field.opened)
+
+// Condição de vitória
+const wonGame = board => fields(board).filter(pendding).lenght === 0
+
+// Mostrar as minas
+const showMines = board => fields(board).filter(field => field.mines)
+     .forEach((field) => field.opened = true)
+
+export {
+     createMinedBoard,
+     cloneBoard,
+     openField,
+     hadExplosion,
+     wonGame,
+     showMines
+}
